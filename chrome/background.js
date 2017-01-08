b04d9ba60493b063afd7e5b37c88dcc44f0f55b8
Alexander Anich <alexander.anich@gmail.com>
2017-01-07 15:46:03 -1000

    basic functionality working for communication

--- chrome/background.js
+++ chrome/background.js
@@ -1,20 +1,35 @@
-function sendMessage(tabId, url) {
-  chrome.storage.sync.get('lovedOnesNumber', function(items) {
-    var number = items.lovedOnesNumber;
-    if (number) {
-      var default_handler = number.length == 0;
+var API_ENDPOINT = "https://zorc2ihpm1.execute-api.us-east-1.amazonaws.com/dev/api/sendText";
 
-      if (!default_handler) {
-        console.log('number', number);
-        console.log('url', url);
-        chrome.storage.sync.remove(tabId);
-      }
+function sendMessage(tabId, url) {
+  chrome.storage.sync.get(null, function(items) {
+    var number = items.lovedOnesNumber;
+    var nastyStorage = items[ tabId ];
+    if (number && number.length > 0 && nastyStorage) {
+      var message = 'Your child has been viewing this site: ' + nastyStorage;
+      var payload = JSON.stringify({
+        to: number,
+        message: message
+      });
+
+      $.ajax({
+        headers: {
+          "Content-type": "application/json"
+        },
+        method: "POST",
+        data: payload,
+        url: API_ENDPOINT
+      })
+      .done(function( data ) {
+        console.log(data);
+      });
+
     }
+    chrome.storage.sync.remove(tabId);
   });
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
+  if (changeInfo.status === 'complete' && tab.incognito) {
     var tabObject = {};
     tabObject[ tabId ] = tab.url;
     chrome.storage.sync.set(tabObject);
