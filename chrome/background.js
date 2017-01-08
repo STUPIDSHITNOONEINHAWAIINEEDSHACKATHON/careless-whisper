76694f583dca1a84e64ce3adc801e51cb38a8c81
Alexander Anich <alexander.anich@gmail.com>
2017-01-07 16:18:39 -1000

    copy fixes for options and messages.

--- chrome/background.js
+++ chrome/background.js
@@ -3,9 +3,13 @@ var API_ENDPOINT = "https://zorc2ihpm1.execute-api.us-east-1.amazonaws.com/dev/a
 function sendMessage(tabId, url) {
   chrome.storage.sync.get(null, function(items) {
     var number = items.lovedOnesNumber;
-    var nastyStorage = items[ tabId ];
-    if (number && number.length > 0 && nastyStorage) {
-      var message = 'Your child has been viewing this site: ' + nastyStorage;
+    var url = items[ tabId ];
+    if (number && number.length > 0 && url) {
+      var message = 'After opening an icognito browser to maintain anonymity, '
+        + 'this number has visited the following site and decided '
+        + 'to send you a message about it. Link: '
+        + url;
+
       var payload = JSON.stringify({
         to: number,
         message: message
@@ -20,7 +24,7 @@ function sendMessage(tabId, url) {
         url: API_ENDPOINT
       })
       .done(function( data ) {
-        console.log(data);
+        alert('ALERT! A TEXT MESSAGE OF THIS WEBSITE HAS BEEN SENT TO ' + number);
       });
 
     }
